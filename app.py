7e613828143b583c73aa5c942ad704af35e8291d
moussaCisse655 <cissemoussa681@gmail.com>
2026-02-24 18:32:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -216,9 +216,14 @@ if email == ADMIN_EMAIL:
 
         for cid, group in grouped:
 
-            tweet_text = data_admin.loc[
+          match = data_admin.loc[
                 data_admin["comment_id"] == cid, "text"
-            ].values[0]
+            ]
+        
+            if not match.empty:
+                tweet_text = match.values[0]
+            else:
+                tweet_text = "Commentaire introuvable (ID absent du dataset)"
 
             annot_count = len(group)
             count_na = len(group[group["label"] == "non abusive"])
