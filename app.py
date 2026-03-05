dcae77eef6e17018a2340bb704703a33a82b4b11
moussaCisse655 <cissemoussa681@gmail.com>
2026-03-05 10:28:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import pandas as pd
 import os
 import hashlib
 import gspread
+import streamlit as st
 from google.oauth2 import service_account
 
-import streamlit as st
+# récupérer les secrets
+info = dict(st.secrets["gcp_service_account"])
 
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
+# corriger les retours à la ligne de la clé privée
+info["private_key"] = info["private_key"].replace("\\n", "\n")
+
+# créer les credentials
+credentials = service_account.Credentials.from_service_account_info(info)
 
 # ---------------- CONFIG ----------------
 DATA_FILE = "data.csv"
